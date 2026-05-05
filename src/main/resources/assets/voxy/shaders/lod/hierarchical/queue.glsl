3883b596eab591b9647e89c28966dbe865a98fa1
SpinGiantCRM <155526553+SpinGiantCRM@users.noreply.github.com>
2026-05-05 18:42:34 +0930

    Fix traversal shader debug macros and improve compile diagnostics

--- src/main/resources/assets/voxy/shaders/lod/hierarchical/queue.glsl
+++ src/main/resources/assets/voxy/shaders/lod/hierarchical/queue.glsl
@@ -1,12 +1,5 @@
 #define SENTINAL_OUT_OF_BOUNDS uint(-1)
 
-#ifdef VOXY_ENABLE_SHADER_DEBUG_PRINTF
-#extension GL_EXT_debug_printf : enable
-#define VOXY_DEBUG_PRINTF(...) printf(__VA_ARGS__)
-#else
-#define VOXY_DEBUG_PRINTF(...)
-#endif
-
 
 layout(binding = NODE_QUEUE_INDEX_BINDING, std430) restrict readonly buffer NodeQueueIndex {
     uint queueIdx;
@@ -38,7 +31,6 @@ void pushNodesInit(uint nodeCount) {
     //Debug
     #ifdef DEBUG
     if (queueIdx >= (MAX_ITERATIONS-1)) {
-        VOXY_DEBUG_PRINTF("LOG: Traversal tried inserting a node into next iteration, which is outside max iteration bounds. GID: %d, count: %d", gl_GlobalInvocationID.x, nodeCount);
         nodePushIndex = -1;
         return;
     }
@@ -54,7 +46,6 @@ void pushNodesInit(uint nodeCount) {
 void pushNode(uint nodeId) {
     #ifdef DEBUG
     if (nodePushIndex == -1) {
-        VOXY_DEBUG_PRINTF("LOG: Tried pushing node when push node wasnt successful. GID: %d, pushing: %d", gl_GlobalInvocationID.x, nodeId);
         return;
     }
     #endif
